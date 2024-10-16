1a7e4702b189fe59c53b5c17c69f8b5569379de5
Lin-CJ-2024 <1046965257@qq.com>
2024-10-16 20:57:07 +0800

    Update PCoA.R

--- PCoA.R
+++ PCoA.R
@@ -1,5 +1,5 @@
 rm(list=ls())#clear Global Environment
-setwd('C:/Users/user/Desktop/α-diversity/马铃薯 BOLD/扩增子测序数据下游分析合集')#设置工作路径
+setwd('')#设置工作路径
 
 #安装所需R包
 # install.packages("vegan")
@@ -72,66 +72,3 @@ p1 <-ggplot(data=df,aes(x=V1,y=V2,
         axis.text.x=element_text(size=10),#修改y轴刻度标签文本
         panel.grid=element_blank())#隐藏网格线
 p1
-
-##########PCoA图添加箱线图######
-#在PCA图的x和y轴添加箱线图，可以实现进一步展示组间差异
-colnames(df) <- c("samples", "PC1", "PC2", "group")#修改数据列名
-#加载包，对组间进行统计检验以及组合图的拼接
-library(ggpubr)
-library(ggsignif)
-# 绘制y轴为PC2值的分组箱线图
-p2 <- ggplot(df,aes(x=group,y=PC2))+#指定数据
-  stat_boxplot(geom = "errorbar", width=0.1,linewidth=0.5)+#添加误差线,注意位置，放到最后则这条线不会被箱体覆盖
-  geom_boxplot(aes(fill=group), #绘制箱线图函数
-               outlier.colour="white",size=0.5)+#异常点去除
-  theme(panel.background =element_blank(), #背景
-        axis.line=element_line(color = "white"),#坐标轴的线设为显示
-        axis.text.y = element_blank(),
-        panel.grid = element_blank(),
-        axis.ticks = element_blank(),#关闭刻度
-        legend.position = 'none')+
-  xlab("") + ylab("")+
-  scale_fill_manual(values=c("#34bf49","#ff4c4c","#0099e5","#FFB90F","#9370DB"))+#指定颜色
-  geom_signif(comparisons = list(c("M1", "M2"), c("M1", "M3"), c("M1", "M4"), c("M1", "M5"), c("M2", "M3"), c("M2", "M4"),
-                                 c("M2","M5"),c("M3","M5"),c("M4","M5")),# 设置需要比较的组
-              map_signif_level = T, #是否使用星号显示
-              test = t.test, ##计算方法
-              y_position = c(1.1,1.2,1.3,1.5,1.6,1.7,1.8,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4),#图中横线位置 设置
-              tip_length = c(c(0,0),
-                             c(0,0),
-                             c(0,0),
-                             c(0,0),
-                             c(0,0),
-                             c(0,0)),#横线下方的竖线设置
-              size=0.8,color="black")
-p2
-# 绘制y轴为PC1值的分组箱线图
-p3 <- ggplot(df,aes(x=group,y=PC1))+#指定数据
-  stat_boxplot(geom = "errorbar", width=0.1,linewidth=0.5)+#添加误差线,注意位置，放到最后则这条线不会被箱体覆盖
-  coord_flip()+
-  geom_boxplot(aes(fill=group), #绘制箱线图函数
-               outlier.colour="white",size=0.5)+#异常点去除
-  theme(panel.background =element_blank(), #背景
-        axis.line=element_line(color = "white"),#坐标轴的线设为显示
-        axis.text.x = element_blank(),
-        panel.grid = element_blank(),
-        axis.ticks = element_blank(),#关闭刻度
-        legend.position = 'none')+
-  xlab("") + ylab("")+
-  scale_fill_manual(values=c("#34bf49","#ff4c4c","#0099e5","#FFB90F"))+#指定颜色
-  geom_signif(comparisons = list(c("A", "B"), c("A", "C"), c("A", "D"), c("B", "C"), c("B", "D"), c("C", "D")),# 设置需要比较的组
-              map_signif_level = T, #是否使用星号显示
-              test = t.test, ##计算方法
-              y_position = c(0.48,0.55,0.62,0.73,0.80,0.48),#图中横线位置 设置
-              tip_length = c(c(0,0),
-                             c(0,0),
-                             c(0,0),
-                             c(0,0),
-                             c(0,0),
-                             c(0,0)),#横线下方的竖线设置
-              size=0.8,color="black")
-p3
-# ggpubr::ggarrange()函数对图进行拼接
-ggarrange(p3, NULL, p1, p2, widths = c(5,2), heights = c(2,5), align = "hv")
-#保存图片
-ggsave('fig/PCoA.pdf',width=12,height = 10)
