2173a6ee8eefc700e5e4d92f312e7b3a79bc9f00
Lin-CJ-2024 <1046965257@qq.com>
2024-10-16 20:58:44 +0800

    Update 阿尔法.R

--- 阿尔法.R
+++ 阿尔法.R
@@ -1,6 +1,6 @@
 
 #设置文件路径
-setwd("C:/Users/user/Desktop/α-diversity/马铃薯 BOLD") 
+setwd("") 
 rm(list = ls())
 
 #导入需要的包
@@ -50,7 +50,7 @@ write.csv(data,"5-alpha data.csv")
 ###以Shannon指数为例画箱线图（即上文图一），需要其他指数替换Shannon即可
 shapiro.test(Simpson)
 #my_comparisons <- list(c("Da", "Sha"), c("Da", "Su"), c("Da", "Xi"), c("Sha", "Xi"), c("Sha", "Su"), c("Su", "Xi"))
-Shannon<- ggplot(data,aes(x=group,y=Chao))+  
+Shannon<- ggplot(data,aes(x=group,y=Shannon))+  
   stat_boxplot(geom = "errorbar", linewidth=0.2)+  #添加误差线
   geom_boxplot(aes(fill=group),linewidth=0.2)+  #箱线图
   scale_fill_manual(values =c("#34bf49","#ff4c4c","#0099e5","#FFB90F","#9370DB"))+
@@ -65,32 +65,6 @@ Shannon<- ggplot(data,aes(x=group,y=Chao))+
         axis.title.y = element_text(vjust=0.2,size=16,face="bold"),
         strip.text = element_text(size = 16),
         legend.position ="none")+ylab(NULL)+
-  facet_grid(.~"Chao")
+  facet_grid(.~"Shannon")
 Shannon
 
-#导出
-ggsave("5-Shannon.png",plot = Shannon,device = "png",height =9,width =16)
-
-###所有指数画分面图（上文图二）
-data_long <- tidyr::gather(data, variable, value, -sample, -group) #先转换数据框为长格式
-
-#画图
-total<-ggplot(data_long, aes(x = group, y = value,color=group)) +
-  stat_boxplot(geom = "errorbar", linewidth=2)+
-  geom_boxplot(linewidth=2)+
-  scale_color_manual(values =c('#ca0020','#f4a582','#92c5de','#0571b0'))+
-  geom_jitter(width = 0.1,alpha =0.5,size=2)+   
-  facet_grid(variable~., scales = "free_y") + #按variable纵向分面
-  labs(title = "alpha diversity", x = "", y = "")+
-  stat_compare_means(method="t.test",label = "p.signif",size=5,vjust = 0.5,hjust=1.3)+theme_bw()
-  theme(panel.background = element_blank(),
-        panel.grid = element_blank(), 
-        strip.text.y = element_text(size = 15, colour = "black",face = "bold"), #分面标题文字设置
-        plot.title = element_text(hjust = 0.5,size=30,face = "bold"),
-        axis.text.x = element_text(size=15,colour ="black",face = "bold"),
-        axis.text.y = element_text(size=10,colour ="black"),
-        legend.position = "none")
-
-total
-#导出
-ggsave("total alpha.png",plot = total,device = "png",height =16,width =9)
